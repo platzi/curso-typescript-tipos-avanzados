67bc48012ca553013252fd8bf04428ad7828ccb4
Nicolas Molina Monroy <hi@nicobytes.com>
2022-02-02 19:40:53 -0400

    :construction: interfaces

---
+++ src/10-interfaces.ts
@@ -0,0 +1,22 @@
+type Sizes = 'S' | 'M' | 'L' | 'XL';
+type userId = string | number;
+interface Product {
+  id: string | number;
+  title: string;
+  createdAt: Date;
+  stock: number;
+  size?: Sizes;
+}
+
+const products: Product[] = [];
+
+products.push({
+  id: '1',
+  title: 'p1',
+  createdAt: new Date(),
+  stock: 90
+});
+
+const addProduct = (data: Product) => {
+  products.push(data);
+}
