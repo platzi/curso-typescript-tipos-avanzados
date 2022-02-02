e5cc17e42c8dcfff93d21dc0c600dc53fe645442
Nicolas Molina Monroy <hi@nicobytes.com>
2022-02-02 17:32:50 -0400

    :construction: default

---
+++ src/05-optionals.ts
@@ -0,0 +1,24 @@
+export const createProduct = (
+  id: string | number,
+  isNew?: boolean,
+  stock?: number,
+) => {
+  return {
+    id,
+    stock: stock ?? 10,
+    isNew: isNew ?? true
+  }
+}
+
+// 0 === false
+// '' === false
+// false === false
+
+const p1 = createProduct(1, true, 12);
+console.log(p1);
+
+const p2 = createProduct(1);
+console.log(p2);
+
+const p3 = createProduct(1, false, 0);
+console.log(p3);
